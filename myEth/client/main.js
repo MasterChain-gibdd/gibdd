13e1e91bffb2fba90693bd7f749b85be6fc82d9d
Vladimir <belyaev_vladimir@outlook.com>
2018-07-13 11:37:47 +0300

    Fixes

--- myEth/client/main.js
+++ myEth/client/main.js
@@ -2,11 +2,80 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
 
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+Template.info.onCreated(function() {
+  var template = this;
+  web3.eth.getAccounts(function(err, res){
+      TemplateVar.set(template, "account", res[0]);
+      web3.eth.getBalance(res[0], function(err, res){
+        TemplateVar.set(template, "balance", res)
+    });
+  });
+  myContract.num(function (err, res) {
+    TemplateVar.set(template, "num", res);
+  });
+});
+
+Template.info.events({
+  'click button'(event, instance) {
+    let template = Template.instance();
+    // syncFunc = Meteor._wrapAsync(myContract.bet);
+    // res = syncFunc();
+    // console.log(res);
+    myContract.bet(function(err, res) {
+      alert(res);
+    })
+  }
+})
+
+/*emp = new Meteor.Collection("Employees");
+Template.form.events({
+  'submit #insert-form':function(e,t){
+    e.preventDefault();
+    var name = t.find('#name').value;
+    var salary = t.find('#salary').value;
+    emp.insert({name:name, salary:salary});
+  }
+}
+)*/
+
+testBase = new Mongo.Collection("testBase");
 userBase = new Mongo.Collection("users");
 carBase = new Mongo.Collection("cars");
 
+Template.myTemplate.events({
 
+  'submit form': function(event) {
+     event.preventDefault();
+     var textValue = event.target.myForm.value;
+     testBase.insert({data:textValue});
+     console.log(textValue);
+     event.target.myForm.value = "";
+  }
+});
+
+Template.myTemplate.helpers({
+  'records':function(){
+    return testBase.find({});
+  }
+})
 
 
 
@@ -15,10 +84,10 @@ Template.userAdder.events({
     event.preventDefault();
     var getBc = event.target.bcAddress.value;
     var getName = event.target.Name.value;
-    funcContract.taskfunc("addUser",web3.eth.accounts[0],getBc,getName,"", function(err, res){
+    addUserContract.addUser("lolo",getBc,getName, function (err, res) {
       TemplateVar.set(template, "num", res);
     }); 
-    Meteor.call('addUser',getBc, getName);
+    userBase.insert({bcAddress:getBc, name:getName, adminRights:false, staffRights:false});
     console.log("user: " + getName);
     event.target.bcAddress.value = "";
     event.target.Name.value = "";
@@ -36,7 +105,8 @@ Template.userSearcher.events({
     event.preventDefault();
     var getBc = event.target.bcAddress.value;
     var getName = event.target.Name.value;
-      funcContract.taskfunc("searchUser",web3.eth.accounts[0],getBc,getName,"", function(err, res){
+    addUserContract.addUser("lolo",getBc,getName, function (err, res) {
+      
     if(!err){
     var searchDict = {};
     if (getBc != ""){
@@ -49,8 +119,8 @@ Template.userSearcher.events({
     document.getElementById("p3").innerHTML = "";
     for (let value of requestResult) {
       document.getElementById("p3").innerHTML += "<p>Address: " + value.bcAddress + " Name: " + value.name + "</p>";
-    }
-    Meteor.call('findUser', getBc, getName);
+    } 
+    //userBase.find({bcAddress:getBc, name:getName});
     console.log("user: " + getName);
   
 }
@@ -84,23 +154,12 @@ Template.addCar.events({
     event.preventDefault();
     var getBuyer = event.target.buyerAddress.value;
     var getVIN = event.target.carVIN.value;
-    funcContract.taskfunc("addCar",web3.eth.accounts[0],getBuyer,getVIN,"", function(err, res){
-      if(!err){
-    Meteor.call('addCar',getBuyer,getVIN);
+    carBase.insert({owner:getBuyer, VIN:getVIN, active:true});
     console.log("owner " + getBuyer + " VIN " + getVIN);
-      }    
-    });
     event.target.buyerAddress.value = "";
     event.target.carVIN.value = "";
   }
 });
-
-Template.addCar.helpers({
-  'cars':function(){
-    return carBase.find({});
-  }
-})
-
 
 
 Template.searchCarHistory.events({
@@ -112,19 +171,12 @@ Template.searchCarHistory.events({
     if (getVIN != ""){
       searchDict["VIN"] = getVIN;
     }
-    funcContract.taskfunc("searchCarHistory",web3.eth.accounts[0],"",getVIN,"", function(err, res){
-      if(!err){
     requestResult = carBase.find(searchDict).fetch()
-    if(requestResult == undefined){
-      console.log('No data by request');
-      return;
-    }
     document.getElementById("p4").innerHTML = "";
     for (let value of requestResult) {
-      document.getElementById("p4").innerHTML += "<p><b>ПТС</b>: " + value.VIN + "<b> Владелец</b>: " + value.owner + "</p>";
-    }
-  }
-});
+      document.getElementById("p4").innerHTML += "<p>Address: " + value.VIN + " Name: " + value.owner + "</p>";
+    } 
+    //userBase.find({bcAddress:getBc, name:getName});
     console.log("car: " + getVIN);
   
 
@@ -143,96 +195,100 @@ Template.modifyCarOwner.events({
     if (getVIN != ""){
       searchDict["VIN"] = getVIN;
     }
-    searchDict["active"] = "да";
-    funcContract.taskfunc("modifyCarOwner",web3.eth.accounts[0],getold,getVIN,getnew, function(err, res){
-      if(!err){
     requestResult = carBase.find(searchDict).fetch();
-    if (requestResult!=undefined){
+    if (requestResult.length != 0){
       carBase.update(requestResult[0]._id,
-        { $set: { active: "нет" } 
-      });
+        { $set: { active: false } });
+      requestResult[0]._id = new mongoose.Types.ObjectId();
       requestResult[0].owner = getnew;
-      carBase.insert({owner:requestResult[0].owner, active:"да", VIN:requestResult[0].VIN});
+      
     console.log("car: " + getVIN);
-    
-  } else{
-    console.log("NO SUCH PTS");
   }
-}
-    });
 
     event.target.carVIN.value = "";
   
   }
 });
 
+var fileId = mongoose.Types.ObjectId();
+
+const contractAddress = "0xdaf4dfc26bdeeea119e2c3cd1e513f001882d57e";
+
+const contractABI = [
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "bet",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "num",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
 
 
-
-const cABI = [
+const addrABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "who",
+				"type": "string"
+			},
+			{
+				"name": "adr",
+				"type": "string"
+			},
+			{
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "addUser",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 	{
 		"anonymous": false,
 		"inputs": [
 			{
 				"indexed": false,
-				"name": "funcname",
+				"name": "who",
 				"type": "string"
 			},
 			{
 				"indexed": false,
-				"name": "user",
+				"name": "adr",
 				"type": "string"
 			},
 			{
 				"indexed": false,
-				"name": "arg1",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "arg2",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "arg3",
+				"name": "name",
 				"type": "string"
 			}
 		],
-		"name": "task",
+		"name": "userAdded",
 		"type": "event"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "funcname",
-				"type": "string"
-			},
-			{
-				"name": "user",
-				"type": "string"
-			},
-			{
-				"name": "arg1",
-				"type": "string"
-			},
-			{
-				"name": "arg2",
-				"type": "string"
-			},
-			{
-				"name": "arg3",
-				"type": "string"
-			}
-		],
-		"name": "taskfunc",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
 	}
 ];
-const cAddress = "0xdf41cf15cec8bd811e39fd66482bb057141c6ba2";
+const addcAddr = "0x6fd0ebfbef686e8dcc3c0091b34288428cd72036";
+const addUserContract = web3.eth.contract(addrABI).at(addcAddr);
 
-const funcContract = web3.eth.contract(cABI).at(cAddress);
+const myContract = web3.eth.contract(contractABI).at(contractAddress);
+
